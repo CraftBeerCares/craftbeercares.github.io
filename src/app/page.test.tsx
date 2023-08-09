64a753fabb324652a4a555869ac55f5b5b60cb35
Gautam Bhatnagar <gautam.bhatnagar@theboyknowsclass.com>
2023-08-09 15:29:42 +0100

    add next-js app

---
+++ src/app/page.test.tsx
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react'
+import Page from './page'
+ 
+it('renders page', () => {
+  const { container } = render(<Page />)
+  expect(container).toMatchSnapshot()
+})
---
+++ src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1>TEST 2</h1>
+}
