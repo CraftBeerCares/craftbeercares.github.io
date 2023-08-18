5922ab03e6897be9e8bc09540103572ae55feae1
Gautam Bhatnagar <gautam.bhatnagar@theboyknowsclass.com>
2023-08-18 23:09:46 +0100

    shrug

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 import PageTemplate from "@/components/pageTemplate";
 import type { Metadata } from "next";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Contact",
 };
 
