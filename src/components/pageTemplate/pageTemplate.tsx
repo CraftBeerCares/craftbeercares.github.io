d961608cf37eb48169d2989e67a90ac831433e5e
G <gautam.bhatnagar@theboyknowsclass.com>
2023-09-07 10:13:37 +0100

    add react carousel (#12)

--- src/components/pageTemplate/pageTemplate.tsx
+++ src/components/pageTemplate/pageTemplate.tsx
@@ -11,13 +11,19 @@ export const PageTemplate = ({ children }: PageTemplateProps) => {
   return (
     <div className={styles.pageContainer}>
       <div className={styles.header}>
-        <Header />
+        <div className={styles.content}>
+          <Header />
+        </div>
       </div>
-      <div className={styles.contentContainer}>
-        <main className={clsx(primary.className, styles.mainContent)}>
+      <div className={styles.scrollContainer}>
+        <main className={clsx(primary.className, styles.content)}>
           {children}
         </main>
-        <Footer />
+        <div className={styles.footer}>
+          <div className={styles.content}>
+            <Footer />
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/testimonials/testimonials.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import Quote from "../quote/quote";
+import testimonials from "./testimonials.json";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+  DotGroup,
+} from "pure-react-carousel";
+import styles from "./testimonials.module.css";
+import "pure-react-carousel/dist/react-carousel.es.css";
+import { useEffect, useRef, useState } from "react";
+
+function Testimonials() {
+  const elementRef = useRef<HTMLDivElement>(null);
+
+  const getQuoteNumber = (): number => {
+    const width = elementRef.current?.offsetWidth ?? window.innerWidth;
+    return window.matchMedia("(min-width: 701px)").matches
+      ? Math.min(Math.floor(width / 420), 3)
+      : 1;
+  };
+
+  const [quoteNumber, setQuoteNumber] = useState(1);
+
+  const setQuoteNumberEffect = () => {
+    const newQuoteNumber = getQuoteNumber();
+
+    if (newQuoteNumber !== quoteNumber) {
+      setQuoteNumber(newQuoteNumber);
+    }
+  };
+
+  // listen for resize events
+  useEffect(() => {
+    setQuoteNumberEffect();
+
+    window.addEventListener("resize", setQuoteNumberEffect);
+    return () => {
+      window.removeEventListener("resize", setQuoteNumberEffect);
+    };
+  }, []);
+
+  return (
+    <CarouselProvider
+      naturalSlideHeight={105}
+      naturalSlideWidth={102}
+      totalSlides={testimonials.length}
+      visibleSlides={quoteNumber}
+      isIntrinsicHeight={true}
+      infinite={true}
+    >
+      <Slider>
+        {testimonials.map(({ quote, author }, index) => (
+          <Slide index={index} key={index} className={styles.quoteBox}>
+            <Quote quote={quote} author={author} />
+          </Slide>
+        ))}
+      </Slider>
+      <div className={styles.controls} ref={elementRef}>
+        <ButtonBack className={styles.button}>Back</ButtonBack>
+        <DotGroup className={styles.dots} />
+        <ButtonNext className={styles.button}>Next</ButtonNext>
+      </div>
+    </CarouselProvider>
+  );
+}
+
+export default Testimonials;
